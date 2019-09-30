9553fe29378844d9a2df2d1625d3640ac703ba2e
Hans-Dieter Stich <hans-dieter@stich.io>
2019-09-30 15:31:37 +0200

    Update settings on all commits

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ module.exports = (robot, _, Settings = require('./lib/settings')) => {
     const payload = context.payload
     const defaultBranch = payload.ref === 'refs/heads/' + payload.repository.default_branch
 
+    if (!defaultBranch) {
+      // Not the default branch, nothing to see here!
+      return;
+    }
     const config = await context.config('settings.yml', {}, { arrayMerge: mergeArrayByName })
 
-    const settingsModified = payload.commits.find(commit => {
-      return commit.added.includes(Settings.FILE_NAME) ||
-        commit.modified.includes(Settings.FILE_NAME)
-    })
-
-    if (defaultBranch && settingsModified) {
       return Settings.sync(context.github, context.repo(), config)
-    }
   })
 }
